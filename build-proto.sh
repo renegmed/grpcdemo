3c4191cecb7eedefee7777998851bac556914c6d
rene <renegmed@yahoo.com>
2018-12-12 10:43:07 -0500

    generating go protobuffer source code by call build-proto.sh

---
+++ build-proto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install proto3 from source
+#  brew install autoconf automake libtool
+#  git clone https://github.com/google/protobuf
+#  ./autogen.sh ; ./configure ; make ; make install
+#
+# Update protoc Go bindings via
+#  go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+#
+# See also
+#  https://github.com/grpc/grpc-go/tree/master/examples
+
+protoc --proto_path $GOPATH/src/grpc-demo/pb messages.proto --go_out=plugins=grpc:$GOPATH/src
